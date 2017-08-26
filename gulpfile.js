3b8b5a68e7415bb038fcb95257fbd71d82aa03a1
Antony Moss <antonymoss.web@gmail.com>
2017-08-26 12:40:55 +0100

    added comments to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,38 @@ var APPPATHS = {
   js: 'app/js'
 }
 
-//=========================================================
+/*=========================================================
+RUN SASS & BROWSER SYNC
+=========================================================*/
+
+gulp.task('sass', function() {
+  return gulp.src(SOURCEPATHS.sassSource)                                      //IMPORTANT ORDER
+    .pipe(autoPrefixer())                                                      //css auto prefix css3
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))          //expand sass output
+    .pipe(gulp.dest('app/css'));                                               //where the sass will be compiled to
+})
+
+gulp.task('server', ['sass'], function() {
+  browserSync.init([APPPATHS.css + '/*.css', APPPATHS.root + '/*.html', APPPATHS.js + '/*.js'], {
+    server: {
+      baseDir: APPPATHS.root
+    }
+  })
+});
+
+/*=========================================================
+COPY FILES FROM THE SRC REPO & DETECT WHEN DELETED
+=========================================================*/
+
+gulp.task('copy', ['clean-html'], function () {
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATHS.root))
+})
+
+gulp.task('scripts', ['clean-scripts'], function () {
+  gulp.src(SOURCEPATHS.jsSource)
+    .pipe(gulp.dest(APPPATHS.js))
+})
 
 gulp.task('clean-html', function () {
   return gulp.src(APPPATHS.root + '/*.html', {read: false, force: true })      //delete files from the root globally
@@ -30,30 +61,9 @@ gulp.task('clean-scripts', function () {
     .pipe(clean());
 })
 
-gulp.task('sass', function() {
-  return gulp.src(SOURCEPATHS.sassSource)                                      //IMPORTANT ORDER
-    .pipe(autoPrefixer())                                                      //css auto prefix css3
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))          //expand sass output
-    .pipe(gulp.dest('app/css'));                                               //where the sass will be compiled to
-})
-
-gulp.task('scripts', ['clean-scripts'], function () {
-  gulp.src(SOURCEPATHS.jsSource)
-    .pipe(gulp.dest(APPPATHS.js))
-})
-
-gulp.task('copy', ['clean-html'], function () {
-  gulp.src(SOURCEPATHS.htmlSource)
-    .pipe(gulp.dest(APPPATHS.root))
-})
-
-gulp.task('server', ['sass'], function() {
-  browserSync.init([APPPATHS.css + '/*.css', APPPATHS.root + '/*.html', APPPATHS.js + '/*.js'], {
-    server: {
-      baseDir: APPPATHS.root
-    }
-  })
-});
+/*=========================================================
+WATCH ALL FUNCTIONS & PASS THROUGH THE 'default'
+=========================================================*/
 
 gulp.task('watch', ['server', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts' ], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
