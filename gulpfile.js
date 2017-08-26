c67a5123f3f6cd93693df3dedebfd68ee47e6c12
Antony Moss <antonymoss.web@gmail.com>
2017-08-26 11:17:37 +0100

    tidying the code

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 var SOURCEPATHS = {
-  sassSource: 'src/scss/*.scss'//* any file in the folder
+  sassSource: 'src/scss/*.scss' //* any file in the folder
 }
 
 var APPPATHS = {
@@ -13,23 +13,26 @@ var APPPATHS = {
   css: 'app/css',
   js: 'app/js'
 }
+//=========================================================
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src(SOURCEPATHS.sassSource)
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));//where the sass will be compiled to
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('app/css')); //where the sass will be compiled to
 })
 
-
-gulp.task('server', ['sass'], function(){
+gulp.task('server', ['sass'], function() {
   browserSync.init([APPPATHS.css + '/*.css', APPPATHS.root + '/*.html', APPPATHS.js + '/*.js'], {
     server: {
-      baseDir : APPPATHS.root
+      baseDir: APPPATHS.root
     }
   })
 });
 
-gulp.task('watch', ['server', 'sass'], function () {
+gulp.task('watch', ['server', 'sass'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
 });
-gulp.task('default', ['watch']);//adds the tasks to the default request
+
+gulp.task('default', ['watch']); //adds the tasks to the default request
