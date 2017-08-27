af345954a74e363001e8f5e2acc9a648a8f47047
Antony Moss <antonymoss.web@gmail.com>
2017-08-27 11:41:11 +0100

    changed the readme.md and .gitignore file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# workflow
-My workflow with NPM, SASS, GULP and more
+My personal workflow using Gulp.
+My workflow with NPM, SASS, GULP and more..
+
+Instructions:
+
+1: Clone the repository
+
+2: Make sure Gulp, Node, & Git are installed
+
+3: Run npm install to install all the dependencies in the package.json
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 //TABLE OF content
 // 1: IMPORT GULP LIBRARIES
 // 2: OBJECTS
-// 3: RUN SASS & BROWSER SYNC
-// 4: MOVE BOOTSRAP FONTS FROM DEPENDENCY TO WOKRING PROJECT
-// 5: COPY & COMPRESS IMAGES
-// 6: HTML PARTIALS
-// 7: COPY SCRIPTS INTO main.js & THEN COMPRESS INTO main-min.js
-// 8: COPY FILES FROM THE SRC REPO & DETECT WHEN DELETED
-// 9: WATCH ALL FUNCTIONS & PASS THROUGH THE 'default'
+// 3: COMPRESS CSS & HTML
+// 4: BROWSER SYNC
+// 5: MOVE BOOTSRAP FONTS FROM DEPENDENCY TO WOKRING PROJECT
+// 6: COPY & COMPRESS IMAGES
+// 7: HTML PARTIALS
+// 8: COPY SCRIPTS INTO main.js & THEN COMPRESS INTO main-min.js
+// 9: COPY FILES FROM THE SRC REPO & DETECT WHEN DELETED
+// 10: WATCH ALL FUNCTIONS DURING THE PRODUCTION PROCESS
+// 11: EXECUTE PRODUCTION TASK WHEN ALL WORK IS COMPLETE BEFORE UPLOADING TO SERVER
 
 // 1: IMPORT GULP LIBRARIES
 var gulp           = require('gulp');
 var sass           = require('gulp-sass');
 var browserSync    = require('browser-sync');
 var reload         = browserSync.reload;
-var autoPrefixer   = require('gulp-autoprefixer');
+var autoprefixer   = require('gulp-autoprefixer');
 var clean          = require('gulp-clean');
 var concat         = require('gulp-concat');
 var browserify     = require('gulp-browserify');
@@ -25,9 +27,10 @@ var injectPartials = require('gulp-inject-partials');
 var minify         = require('gulp-minify');
 var rename         = require('gulp-rename');
 var cssmin         = require('gulp-cssmin');
+var htmlmin        = require('gulp-htmlmin');
 
 // 2: OBJECTS
-var SOURCEPATHS    = {
+var SOURCE    = {
   sassSource:        'src/scss/*.scss',          //* any file in the folder
   htmlSource:        'src/*.html',
   htmlPartialSource: 'src/partial/*.html',
@@ -35,121 +38,155 @@ var SOURCEPATHS    = {
   imgSource:         'src/img/**'
 }
 
-var APPATHS        = {                           //object containing the repository folders
-  root:              'app/',
-  css:               'app/css',
-  js:                'app/js',
-  fonts:             'app/fonts',
-  img:               'app/img'
+var PROJECT        = {                           //object containing the repository folders
+  root:              'project/',
+  css:               'project/css',
+  js:                'project/js',
+  fonts:             'project/fonts',
+  img:               'project/img'
 }
 
 /*=========================================================
-RUN PRODUCTION TASKS
+3: COMPRESS CSS & HTML
 =========================================================*/
 
-gulp.task('compresscss', function() {
+gulp.task('sass', function() {
   var bootstrapCss = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
   var sassFiles;
-  sassFiles = gulp.src(SOURCEPATHS.sassSource)                                      //IMPORTANT ORDER
-    .pipe(autoPrefixer())                                                      //css auto prefix css3
+
+  sassFiles = gulp.src(SOURCE.sassSource)                                      //IMPORTANT ORDER
+    .pipe(autoprefixer())                                                      //css auto prefix css3
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))          //expand sass output
       return merge(bootstrapCss, sassFiles)
-      .pipe(concat('app.css'))                                                 //concat bootsrap css & custom
-      .pipe(cssmin())                                                          //compress css
-      .pipe(rename({suffix:'.min'}))
-      .pipe(gulp.dest('app/css'));                                             //where the sass will be compiled to
+      .pipe(concat('project.css'))                                                 //concat bootsrap css & custom
+      .pipe(gulp.dest('project/css'));                                             //where the sass will be compiled to
+});
+
+/** Production Tasks **/
+gulp.task('compress',  function() {
+  gulp.src(SOURCE.jsSource)
+      .pipe(concat('main.js'))
+      .pipe(browserify())
+      .pipe(minify())
+      .pipe(gulp.dest(PROJECT.js))
+});
+
+gulp.task('compresscss', function(){
+  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+  var sassFiles;
+  sassFiles = gulp.src(SOURCE.sassSource)
+      .pipe(autoprefixer())
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      return merge(bootstrapCSS, sassFiles)
+          .pipe(concat('project.css'))
+          .pipe(cssmin())
+          .pipe(rename({suffix: '.min'}))
+          .pipe(gulp.dest(PROJECT.css));
+});
+
+gulp.task('minifyHtml', function() {
+   return gulp.src(SOURCE.htmlSource)
+        .pipe(injectPartials())
+        .pipe(htmlmin({collapseWhitespace:true}))
+        .pipe(gulp.dest(PROJECT.root))
 });
 
 /*=========================================================
-BROWSER SYNC
+4: BROWSER SYNC
 =========================================================*/
 
 gulp.task('server', ['sass'], function() {
-  browserSync.init([APPATHS.css + '/*.css', APPATHS.root + '/*.html', APPATHS.js + '/*.js'], {
+  browserSync.init([PROJECT.css + '/*.css', PROJECT.root + '/*.html', PROJECT.js + '/*.js'], {
     server: {
-      baseDir: APPATHS.root
+      baseDir: PROJECT.root
     }
   })
 });
 
 /*=========================================================
-MOVE BOOTSRAP FONTS FROM DEPENDENCY TO WOKRING PROJECT
+5: MOVE BOOTSRAP FONTS FROM DEPENDENCY TO WOKRING PROJECT
 =========================================================*/
 
 gulp.task('moveFonts', function () {
   gulp.src('./node_modules/bootstrap/fonts/*.{eot,svg,ttf,woff,woff2}')        //source
-    .pipe(gulp.dest(APPATHS.fonts))                                           //destination
+    .pipe(gulp.dest(PROJECT.fonts))                                           //destination
 });
 
 /*=========================================================
-COPY & COMPRESS IMAGES
+6: COPY & COMPRESS IMAGES
 =========================================================*/
 
 gulp.task('images', function () {
-  return gulp.src(SOURCEPATHS.imgSource)
-  .pipe(newer(APPATHS.img))
+  return gulp.src(SOURCE.imgSource)
+  .pipe(newer(PROJECT.img))
   .pipe(imagemin())
-  .pipe(gulp.dest(APPATHS.img));
+  .pipe(gulp.dest(PROJECT.img));
 });
 
 /*=========================================================
-HTML PARTIALS
+7: HTML PARTIALS
 =========================================================*/
 
 gulp.task('html', function () {
-  return gulp.src(SOURCEPATHS.htmlSource)
+  return gulp.src(SOURCE.htmlSource)
   .pipe(injectPartials())
-  .pipe(gulp.dest(APPATHS.root))
+  .pipe(gulp.dest(PROJECT.root))
 });
 
 /*=========================================================
-COPY SCRIPTS INTO main.js & THEN COMPRESS INTO main-min.js
+8: COPY SCRIPTS INTO main.js & THEN COMPRESS INTO main-min.js
 =========================================================*/
 
 gulp.task('scripts', ['clean-scripts'], function () {
-  gulp.src(SOURCEPATHS.jsSource)
+  gulp.src(SOURCE.jsSource)
     .pipe(concat('main.js'))                                                   //concat js files into one file called main.js
     .pipe(browserify())                                                        //includes bootsrap, jquery, & mustache js
-    .pipe(gulp.dest(APPATHS.js))
+    .pipe(gulp.dest(PROJECT.js))
 });
 
 gulp.task('compress', function () {
-  gulp.src(SOURCEPATHS.jsSource)
+  gulp.src(SOURCE.jsSource)
     .pipe(concat('main.js'))                                                   //compress js files into one file called main.js
     .pipe(minify())
-    .pipe(gulp.dest(APPATHS.js))
+    .pipe(gulp.dest(PROJECT.js))
 });
 
 /*=========================================================
-COPY FILES FROM THE SRC REPO & DETECT WHEN DELETED
+9: COPY FILES FROM THE SRC REPO & DETECT WHEN DELETED
 =========================================================*/
 
 // gulp.task('copy', ['clean-html'], function () {
-//   gulp.src(SOURCEPATHS.htmlSource)
-//     .pipe(gulp.dest(APPATHS.root))
+//   gulp.src(SOURCE.htmlSource)
+//     .pipe(gulp.dest(PROJECT.root))
 // });
 
 gulp.task('clean-html', function () {
-  return gulp.src(APPATHS.root + '/*.html', {read: false, force: true })      //delete files from the root globally
+  return gulp.src(PROJECT.root + '/*.html', {read: false, force: true })      //delete files from the root globally
     .pipe(clean());
 });
 
 gulp.task('clean-scripts', function () {
-  return gulp.src(APPATHS.js + '/*.js', {read: false, force: true })          //delete files from the root globally
+  return gulp.src(PROJECT.js + '/*.js', {read: false, force: true })          //delete files from the root globally
     .pipe(clean());
 });
 
 /*=========================================================
-WATCH ALL FUNCTIONS & PASS THROUGH THE 'default'
+10: WATCH ALL FUNCTIONS DURING THE PRODUCTION PROCESS
 =========================================================*/
 
 gulp.task('watch', ['server', 'sass', 'clean-html', 'html', 'clean-scripts', 'scripts', 'moveFonts', 'images' ], function() {
-  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
-  // gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
-  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
-  gulp.watch([SOURCEPATHS.imgSource], ['images']);
-  gulp.watch([SOURCEPATHS.htmlSource, SOURCEPATHS.htmlPartialSource], ['html']);
+  gulp.watch([SOURCE.sassSource], ['sass']);
+  // gulp.watch([SOURCE.htmlSource], ['copy']);
+  gulp.watch([SOURCE.jsSource], ['scripts']);
+  gulp.watch([SOURCE.imgSource], ['images']);
+  gulp.watch([SOURCE.htmlSource, SOURCE.htmlPartialSource], ['html']);
 
 });
 
 gulp.task('default', ['watch']);                                               //passes 'watch' through the default
+
+/*=========================================================
+11: EXECUTE PRODUCTION TASK WHEN ALL WORK IS COMPLETE BEFORE UPLOADING TO SERVER
+=========================================================*/
+
+gulp.task('production', ['compress', 'compresscss', 'minifyHtml']);
